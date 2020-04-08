7a28b66a8536f3911bbf68bde5dbe44c0f9b45e1
Emile2020 <emiledebruyckere@gmail.com>
2020-04-08 08:30:45 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+//hier niets document
+var name1 = prompt ("what is your name");
+var name2 = prompt ("what is the name of the loved one");
+
+alert("Your score is above average you are a nice couple");
+alert ("10/10" + " is the love score from " + name1 + " and " + name2);
+alert ("this is realy a good love meter");
+//doe wat je wilt hier
